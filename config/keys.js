c9dde31bc084c9b9068b657bf35d8d7fc3f2d2e4
HaiderAbbas12345 <hmughal0123@gmail.com>
2023-01-11 21:32:53 +0500

    keychanged

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,18 @@
+// const db = {
+//   username: "HaiderAbbas",
+//   password: "7p4KXSgKkTKrDz0h",
+//   database: "storelocator",
+//   cluster: "cluster0",
+// };
+// const uri = `mongodb+srv://${db.username}:${db.password}@${db.cluster}.7smpdm2.mongodb.net/${db.database}?retryWrites=true&w=majority`;
+
 const db = {
-  username: "HaiderAbbas",
-  password: "7p4KXSgKkTKrDz0h",
-  database: "storelocator",
+  username: "usman",
+  password: "usman886",
+  database: "mytable",
   cluster: "cluster0",
 };
-
-// const uri = `mongodb+srv://${db.username}:${db.password}@${db.cluster}.fcmxtgn.mongodb.net/${db.database}?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://${db.username}:${db.password}@${db.cluster}.7smpdm2.mongodb.net/${db.database}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${db.username}:${db.password}@${db.cluster}.ua23z.mongodb.net/${db.database}?retryWrites=true&w=majority`;
 
 module.exports = uri;
+// mongodb+srv://usman:<password>@cluster0.ua23z.mongodb.net/?retryWrites=true&w=majority
